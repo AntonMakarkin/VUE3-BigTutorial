5c08211288efd934c34877748a58b4d7a0e90787
AntonMakarkin <amakarkin27@gmail.com>
2022-12-05 15:40:02 +0300

    Two way binding

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,19 @@
 <template>
   <div class="app">
     <form>
-      <input class="input" type="text" placeholder="Название">
-      <input class="input" type="text" placeholder="Описание">
-      <button class="btn">Создать</button>
+      <input 
+        v-bind:value="title"
+        @input="title = $event.target.value" 
+        class="input" 
+        type="text" 
+        placeholder="Название">
+      <input 
+        v-bind:value="body"
+        @input="body = $event.target.value" 
+        class="input" 
+        type="text" 
+        placeholder="Описание">
+      <button class="btn" @click="createPost">Создать</button>
     </form>
     <div class="post" v-for="post in posts" key:="post.id">
       <div><strong>Название:</strong>{{ post.title }}</div>
@@ -20,7 +30,19 @@
           {id: 1, title: 'JavaScript', body: 'Описание поста'},
           {id: 2, title: 'JavaScript', body: 'Описание поста 2'},
           {id: 3, title: 'JavaScript', body: 'Описание поста 3'},
-        ]
+        ],
+        title: '',
+        body: ''
+      }
+    },
+    methods: {
+      createPost() {
+        const newPost = {
+          id: Date.now(),
+          title: this.title,
+          body: this.body
+        }
+        this.posts.push(newPost)
       }
     }
   }
